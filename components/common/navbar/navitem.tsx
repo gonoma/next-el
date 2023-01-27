9e24ac2d8eb0a2e09284b48cdd4c3ea5d88f304c
Gonzalo Nogueras <gnm12ct@gmail.com>
2023-01-27 00:28:35 +0000

    about page a bit more complete

--- components/common/navbar/navitem.tsx
+++ components/common/navbar/navitem.tsx
@@ -1,21 +1,15 @@
 import Link from "next/link";
 
 type NavProps = {
-    text: string;
-    href: string;
-    active: boolean;
+  text: string;
+  href: string;
+  active: boolean;
 };
 
 const NavItem = ({ text, href, active }: NavProps) => {
   return (
     <Link href={href}>
-      <a
-        className={`nav__item ${
-          active ? "active" : ""
-        }`}
-      >
-        {text}
-      </a>
+      <a className={`nav__item ${active ? "active" : ""}`}>{text}</a>
     </Link>
   );
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function YoutubeEmbed(props: any): any {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={"https://www.youtube.com/embed/" + props.embedID}
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  );
+}
+
+YoutubeEmbed.propTypes = {
+  embedID: PropTypes.string.isRequired,
+};
+
 export default function About() {
-    return(
-        <h1 className="text-center text-green-900 font-bold text-4xl">
-            This is About Page
-        </h1> 
-    )
+  return (
+    // <h1 className="text-center text-green-900 font-bold text-4xl">
+    //   This is About Page
+    // </h1>
+
+    <div className="about-page">
+      <h1 className="text-center text-green-900 font-bold text-4xl">
+        This is About Page
+      </h1>
+      <YoutubeEmbed embedID="xYMqo5fsbPw" />
+      <YoutubeEmbed embedID="cw6k5rYmjs8" />
+    </div>
+  );
 }
